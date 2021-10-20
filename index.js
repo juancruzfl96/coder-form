fcb1d9f44ad9bcb0395bb7ca707f303902f75828
juancruzfl96 <juancruzfl96@github.com>
2021-10-20 11:51:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function information() {
+	let name = document.getElementById('name').value;
+	let lastName = document.getElementById('lastName').value;
+	let mail = document.getElementById('mail').value;
+	let text = document.getElementById('text').value;
+
+	console.log('Name: ' + name + ' ' + lastName);
+	console.log('Email: ' + mail);
+	console.log('Menssage: ' + text);
+}
